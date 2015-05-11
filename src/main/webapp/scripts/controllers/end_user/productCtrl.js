3c1aaac432b9f547f6272096ab8a754e1fd01c2d
JayaramachandranAugustin <jaugustin@agilerulesconsultants.com>
2015-05-12 02:03:22 +0530

    Merged BRMS Cross sell

--- src/main/webapp/scripts/controllers/end_user/productCtrl.js
+++ src/main/webapp/scripts/controllers/end_user/productCtrl.js
@@ -1,35 +1,64 @@
 /**
  * 
  */
-angular.module('enduser').controller('productCtrl',function($scope,$http,$routeParams,AkProductsResource,DataService){
+angular.module('enduser').controller('productCtrl',function($scope,$http,$routeParams,AkProductsResource,AkProductOptionsResource,DataService){
 	$scope.category=$routeParams.category;
 	$scope.prodId=$routeParams.productId;
 	$scope.productList=AkProductsResource.queryAll();
 	var prodlocal=$scope.productList;
-/*	$scope.postObject = new Object();*/
 	$scope.product=function($scope){
 		for(var i=0;i<prodlocal.length;i++){
 			if(prodlocal[i].productId==$routeParams.productId){
-
-				 
-			/*	$scope.postObject.categoryId = prodlocal[i].akProductCategories.categoryId;*/
 				return prodlocal[i];
 			}
 		}
 	};
 	
-	
-	
-	
 	 $scope.cart = DataService.cart;
 	 
+	 var products=[];
+	 $scope.productRest=AkProductsResource.queryAll();
+	 $scope.productOptionsRest=AkProductOptionsResource.queryAll();
+	 $scope.productRest.$promise.then(function(dataProduct){
+		 $scope.productOptionsRest.$promise.then(function(dataProductOptions){
+			 console.log("dataProductOptions"+dataProductOptions);
+			 var postObject = new Object();
+				for(var i=0;i<dataProduct.length;i++){
+					if(dataProduct[i].productId==$routeParams.productId){
+						postObject.categoryId = dataProduct[i].akProductCategories.categoryId;
+					}
+						
+				}
+				for(var j=0;j<dataProductOptions.length;j++){
+					if(dataProductOptions[j].akProducts.productId==$routeParams.productId){
+						console.log("dataProductOptions[j].akProducts.productId"+dataProductOptions[j].akProducts.productId);
+						postObject.optionId=dataProductOptions[j].akOptions.optionId;
+						postObject.optionGroupId=dataProductOptions[j].akOptionGroups.optionGroupId;
+					}
+				}
+				console.log("CategoryId  "+postObject.categoryId);
+				console.log("ProductOptionId "+postObject.optionId);
+				 console.log("OptionGroupId "+postObject.optionGroupId);
+				 $http.post("/AgileKartService/agilekart", postObject).success(function(brms){
+					   $scope.bramscategory=brms.crosellProduct.categoryId;
+					   $scope.bramsoption=brms.crosellProduct.optionId;
+					   $scope.bramsgroupid=brms.crosellProduct.optionGroupId;
+					   products=[];
+					   for(var i=0;i<dataProductOptions.length;i++){
+						   if(brms.crosellProduct.optionId==dataProductOptions[i].akOptions.optionId
+								   &&dataProductOptions[i].akOptionGroups.optionGroupId==brms.crosellProduct.optionGroupId
+								   &&dataProductOptions[i].akProducts.akProductCategories.categoryId==brms.crosellProduct.categoryId){
+							   products.push(dataProductOptions[i].akProducts);
+						   }
+					   }
+					   $scope.productsCrossList=products;
+					   console.log("products"+products);
+					   for(var i=0;i<products.length;i++){
+						   console.log(products[i].productCartDesc);
+					   }
+					});
+		 });
+	 });
 	 
-	/* console.log("$scope.product.akProductCategories.categoryId  "+$scope.postObject.categoryId);
-	 console.log("$scope.product.akProductOptionses.productOptionId "+postObject.optionId);
-	 console.log("$scope.product.akProductOptionses.akOptionGroups.optionGroupId "+postObject.optionGroupId);
-	 $http.post("/AgileKartService/agilekart", postObject).success(function(data){
-		   console.log(data.crosellProduct.categoryId);
-		   console.log(data.crosellProduct.optionId);
-		   console.log(data.crosellProduct.optionGroupId);
-		});*/
+	 
 });
